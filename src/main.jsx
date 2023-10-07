05e0a68c170397bf83e78ca0dcf618291a8dd60a
Mateus Louzada <mateuslouzada01@gmail.com>
2023-10-07 10:40:57 -0300

    Inicial

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { PrimeReactProvider } from "primereact/api";
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primeflex/primeflex.css';                                   
+import 'primeicons/primeicons.css';
+import 'primereact/resources/primereact.css'; 
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
+  </React.StrictMode>
+);
